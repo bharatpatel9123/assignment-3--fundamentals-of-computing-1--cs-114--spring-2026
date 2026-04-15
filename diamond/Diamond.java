3a3f78cdc71378988173a0a769953fc57947ecef
bharatpatel9123 <bharatpatel9123@gmail.com>
2026-04-15 13:53:08 -0400

    added even diamonds and determined if user input is even or odd

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -7,19 +7,55 @@ public class Diamond {
         System.out.println("Enter a number");
         int n = scanner.nextInt();
         
+        int mid = n / 2;
+        if (n % 2 == 0) {
+            for (int i = 0; i <= mid; i++) {
             for (int j = 0; j < mid - i; j++) {
                 System.out.print(" ");
-            for (int j = 0; j < 2 * i + 1; j++) {
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+                if (j < i) {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
+        
+        for (int i = mid - 1; i >= 0; i--) {
+            for (int j = 0; j < mid - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+                if (j < i) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        }
+        else {
+            for (int i = 0; i <= mid; i++) {
+            for (int j = 0; j < mid - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
         for (int i = mid - 1; i >= 0; i--) {
             for (int j = 0; j < mid - i; j++) {
                 System.out.print(" ");
             }
             for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
+            }
             System.out.println();
+        }
+        }
         
         scanner.close();
     }
