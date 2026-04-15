1f56d2f39c621cc0e3484fa61176b488b8cdfc4a
bharatpatel9123 <bharatpatel9123@gmail.com>
2026-04-15 12:58:38 -0400

    Did odd version of diamond

--- diamond/Diamond.java
+++ diamond/Diamond.java
@@ -1,4 +1,26 @@
+import java.util.Scanner;
+
 public class Diamond {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter a number");
+        int n = scanner.nextInt();
+        
+            for (int j = 0; j < mid - i; j++) {
+                System.out.print(" ");
+            for (int j = 0; j < 2 * i + 1; j++) {
+            }
+            System.out.println();
+        }
+        for (int i = mid - 1; i >= 0; i--) {
+            for (int j = 0; j < mid - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            System.out.println();
+        
+        scanner.close();
+    }
 }
