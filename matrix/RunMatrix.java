e780d21d5995762731ebae8a642d99d3e078d7ad
bharatpatel9123 <bharatpatel9123@gmail.com>
2026-04-15 16:55:06 -0400

    Update RunMatrix.java

--- matrix/RunMatrix.java
+++ matrix/RunMatrix.java
@@ -1,6 +1,32 @@
 import java.util.Scanner;
 
 public class RunMatrix {
-  public static void main(String[] args) {
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the size of the matrix: ");
+        int size = scanner.nextInt();
+        
+        while (size <= 0) {
+            System.out.print("Please enter a positive number: ");
+            size = scanner.nextInt();
+        }
+        
+        Matrix matrix = new Matrix(size);
+        
+        System.out.println("\nInitial matrix (all zeros):");
+        matrix.printMatrix();
+        
+        matrix.populateMatrix();
+        
+        System.out.println("Populated matrix:");
+        matrix.printMatrix();
+        
+        matrix.flipMatrix();
+        
+        System.out.println("Flipped matrix (along top-right to bottom-left diagonal):");
+        matrix.printMatrix();
+        
+        scanner.close();
+    }
 }
